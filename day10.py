df1add7c7191ffa2329b5da95db209c0cb088504
suweijia <92003810+suweijia@users.noreply.github.com>
2021-11-01 10:39:56 +0800

    Update day10.py

--- day10.py
+++ day10.py
@@ -22,27 +22,37 @@ class Notebook:
     __time=""
 
     def setscreen(self,screen):
+        self.__screen=screen
         print(screen,"的电脑可以敲代码".format(screen))
     def getscreen(self):
         return  self.__screen
+
     def setmoney(self,money):
+        self.__money=money
         print("这个电脑",money,"元")
     def getmoney(self):
         return self.__money
+
     def setcpu(self,cpu):
+        self.__cpu=cpu
         print(cpu,"的可以玩游戏")
     def getcpu(self):
         return self.__cpu
+
     def setcont(self,cont):
+        self.__cont=cont
         print("{}:不错的内存".format(cont))
     def getcont(self):
         return self.__cont
+
     def settime(self,time):
+        self.__time=time
         print("待机时间：",time)
     def gettime(self):
         return self.__time
+
     def cmd(self):
-        print("{}-{}元-{}-{}电脑可以敲码，打游戏，看视频".format(self.__screen,self.__money,self.__cpu,self.__cont))
+        print("{}-{}元-{}-{}的电脑可以敲码，打游戏，看视频".format(self.__screen, self.__money, self.__cpu, self.__cont))
 
 cm=Notebook()
 cm.setscreen("1920x1920pi")
